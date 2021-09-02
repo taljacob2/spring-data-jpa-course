7b78bd971191e1dd31bacdc6fe5dec1c7d6638bc
Tal Jacob <taljacob2@gmail.com>
2021-09-02 05:46:48 +0300

    after vid 1

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.pojo.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean CommandLineRunner commandLineRun(
+            StudentRepository studentRepository) {
+        return new CommandLineRunner() {
+            @Override public void run(String... args) throws Exception {
+                Student student = new Student();
+                student.setFirstName("Maria");
+                student.setLastName("Jones");
+                student.setEmail("maria.jones@gmail.com");
+                student.setAge(21);
+                studentRepository.save(student);
+            }
+        };
+    }
 }
---
+++ src/main/java/com/example/demo/pojo/Student.java
@@ -0,0 +1,25 @@
+package com.example.demo.pojo;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "Student") @Table(name = "student", uniqueConstraints = {
+        @UniqueConstraint(name = "student_email_unique",
+                columnNames = "email")}) @Data public class Student {
+    @Id @SequenceGenerator(name = "student_sequence",
+            sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence")
+    @Column(name = "id", updatable = false) @Setter(value = AccessLevel.PRIVATE)
+    private Long id;
+    @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
+    private String firstName;
+    @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
+    private String lastName;
+    @Column(name = "email", nullable = false, columnDefinition = "TEXT")
+    private String email;
+    @Column(name = "age", nullable = false) private Integer age;
+}
